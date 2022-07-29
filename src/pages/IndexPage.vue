1a8d4dcb635c78c7db55ddfd91b2c7b1236acf3e
HgaMen <hga.int2021@gmail.com>
2022-07-28 19:21:01 -0500

    ��

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,44 +1,19 @@
 <template>
   <q-page padding>
-    <q-btn @click="access">Ingresar</q-btn>
+    <!-- <q-btn @click="userStore.access">Ingresar</q-btn> -->
     <q-btn @click="createLink">Crear Link</q-btn>
-    {{ token }} - {{ expiresIn }}
+    <!-- <q-btn @click="userStore.logout">Cerrar sesión</q-btn> -->
+    {{ userStore.token }} - {{ userStore.expiresIn }}
   </q-page>
 </template>
 
 <script setup>
 import { api } from "src/boot/axios";
-import { ref } from "vue";
+import { useUserStore } from "../stores/user-store";
 
-// const access = () => {
-//   console.log("me diste click");
-//   axios
-//     .post("http://localhost:5000/api/v1/auth/login", {
-//       email: "rigo@test.com",
-//       password: "123123",
-//     })
-//     .then((res) => {
-//       console.log(res.data);
-//     })
-//     .catch((e) => console.log(e));
-// };
+const userStore = useUserStore();
 
-const token = ref("");
-const expiresIn = ref("");
-
-const access = async () => {
-  try {
-    const res = await api.post("/auth/login", {
-      email: "rigo@test.com",
-      password: "123123",
-    });
-    token.value = res.data.token;
-    expiresIn.value = res.data.expiresIn;
-    setTime();
-  } catch (error) {
-    console.log(error);
-  }
-};
+// userStore.refreshToken();
 
 const createLink = async () => {
   try {
@@ -57,23 +32,4 @@ const createLink = async () => {
     console.log(error);
   }
 };
-
-const setTime = () => {
-  setTimeout(() => {
-    console.log("se refresco");
-    refreshToken();
-  }, expiresIn.value * 1000 - 6000);
-};
-
-const refreshToken = async () => {
-  try {
-    const res = await api.get("/auth/refresh");
-    token.value = res.data.token;
-    expiresIn.value = res.data.expiresIn;
-    setTime();
-  } catch (error) {
-    console.log(error);
-  }
-};
-refreshToken();
 </script>
