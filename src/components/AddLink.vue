4beb6a847b33e2ed0ce4757ba862c5244ce35d6f
HgaMen <hga.int2021@gmail.com>
2022-08-09 18:17:46 -0500

    :pizza::pizza::pizza:

---
+++ src/components/AddLink.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from "vue";
+import { useLinkStore } from "src/stores/link-store.js";
+import { useNotify } from "../composables/notifyHook.js";
+
+const useLink = useLinkStore();
+const { showNotify } = useNotify();
+
+const link = ref("");
+const loading = ref(false);
+
+const addLink = async () => {
+  try {
+    loading.value = true;
+    await useLink.createLink(link.value);
+    showNotify("Link agregado con éxito", "green");
+    link.value = "";
+  } catch (error) {
+    console.log(error.errors);
+    if (error.errors) {
+      error.errors.forEach((item) => {
+        showNotify(item.msg);
+      });
+    }
+    showNotify(error);
+  } finally {
+    loading.value = false;
+  }
+};
+</script>
+
+<template>
+  <q-form @submit.prevent="addLink">
+    <q-input
+      v-model="link"
+      label="Ingrese link aquí"
+      :rules="[(val) => (val && val.trim() !== '') || 'Escribe algo por favor']"
+    ></q-input>
+    <q-btn
+      class="q-mt-sm full-width"
+      label="Agregar"
+      color="primary"
+      type="submit"
+      :loading="loading"
+    ></q-btn>
+  </q-form>
+</template>
